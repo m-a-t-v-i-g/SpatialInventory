2fcb1bda663a02ef1ce2852e5e16cea556a239ee
m-a-t-v-i-g <66176127+m-a-t-v-i-g@users.noreply.github.com>
2023-05-10 17:08:26 +0700

    Inventory Basis Added
    Added: inventory component, widgets and initial logic for drawing inventory.

--- Source/SpatialInventory/Public/Components/InventoryComponent.h
+++ Source/SpatialInventory/Public/Components/InventoryComponent.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Spatial Inventory С++ template by matvig.
 
 #pragma once
 
@@ -6,23 +6,24 @@
 #include "Components/ActorComponent.h"
 #include "InventoryComponent.generated.h"
 
-
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SPATIALINVENTORY_API UInventoryComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UInventoryComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (ExposeOnSpawn = true), Category = "Inventory Size")
+	int Columns = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (ExposeOnSpawn = true), Category = "Inventory Size")
+	int Rows = 1;
+	
 };
